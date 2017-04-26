b5eb383113de28b641a019abba09a747d1aae0cf
faterror <stereodic@gmail.com>
2017-04-26 19:41:19 +1200

    Selecting a time before current is now treated as a the next day. Code cleanup.

--- app/app.request.ts
+++ app/app.request.ts
@@ -38,17 +38,31 @@ export class RequestComponent implements OnInit {
     public loadRemote() {
         this.http.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=` + this.departure + `&destinations=` + this.arrival + `&mode=car&departure_time=` + this.departureTime + `&language=en-EN&key=
         `).map(res => res.json()).subscribe((response: any) => {
-                this.duration = this.cleanString(JSON.stringify(response.rows[0].elements[0].duration.text));
-                this.distance = this.cleanString(JSON.stringify(response.rows[0].elements[0].distance.text));
-                let timePicker: TimePicker = <TimePicker>this.tp.nativeElement;
-                this.arrivalTime.setHours(timePicker.hour, timePicker.minute);
-                var estT = new Date(this.estimatedArrival);
-                this.estimatedArrival = this.departureTime + (response.rows[0].elements[0].duration.value * 1000);
-                var estTC = new Date(this.estimatedArrival);
+                this.processResponse(response);
                 this.compareTimes();
-                this.router.navigate(["/result"], { queryParams: { result: this.callItIn, duration: this.duration, distance: this.distance, eta: estTC, onTime: this.onTime } });
+                this.router.navigate(["/result"], { queryParams: { result: this.callItIn, duration: this.duration, distance: this.distance, eta: new Date(this.estimatedArrival), onTime: this.onTime } });
             });
     }
+
+    public processResponse(response) {
+        this.duration = this.cleanString(JSON.stringify(response.rows[0].elements[0].duration.text));
+        this.distance = this.cleanString(JSON.stringify(response.rows[0].elements[0].distance.text));
+        this.estimatedArrival = this.departureTime + (response.rows[0].elements[0].duration.value * 1000);
+        this.daySwitch();
+    }
+
+    public daySwitch() {
+        let timePicker: TimePicker = <TimePicker>this.tp.nativeElement;
+        this.arrivalTime.setHours(timePicker.hour, timePicker.minute);
+        var currentTime = new Date();
+        var currentHour = timePicker.hour == currentTime.getHours();
+        var futureHour = timePicker.hour < currentTime.getHours();
+        var futureMinute = timePicker.minute < currentTime.getMinutes()
+        if (futureHour || currentHour && futureMinute) {
+            this.arrivalTime.setDate(this.arrivalTime.getDate() + 1); //add 1 day, otherwise it calculates a useless comparison with a time that has already passed
+        }
+    }
+
     public cleanString(string) {
         var cleanString = string.replace(/"/g, '');
         return cleanString;
